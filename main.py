6752ccc7f0ec723acef2618ad1f8174dc674a2d2
Tobias Voth <tobias@voth-mail.de>
2026-09-18 13:54:58 +0200

    add Data access function

--- main.py
+++ main.py
@@ -1,16 +1,8 @@
-# This is a sample Python script.
-
-# Press Umschalt+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Data.access_data import load_data
+import pandas as pd
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Strg+F8 to toggle the breakpoint.
+pegel_data = load_data('2022-01-01')
 
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(pegel_data.head())
+print(pegel_data.describe())
